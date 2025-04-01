90ee8eeb6d021a0410f9389f6e3b294fdef0b85a
Han-s-j <78011872+Han-s-j@users.noreply.github.com>
2025-04-01 23:23:17 +0900

    0401

--- python/bs4_oxford3.py
+++ python/bs4_oxford3.py
@@ -1,11 +1,16 @@
 import os.path
 from os import write
+
+import openpyxl
+import pip
 from bs4 import BeautifulSoup
 import requests
 import csv
 import urllib.request as req
 from selenium import webdriver
 import time
+import pandas as pd
+import openpyxl
 
 url = 'https://www.oxfordlearnersdictionaries.com/definition/english/star_1?q=star'
 driver = webdriver.Chrome()
@@ -21,6 +26,10 @@ div = soup.select_one('#star_1')
 #print(div)
 # 'star_1' div 안에 있는 모든 <ol> 요소를 찾음
 ols = div.find_all('ol')
+
+# 리스트로 예문을 저장
+examples_list = []
+
 #print(ols)
 # 각 <ol> 안에 있는 <ul> 요소들 중에서 예문을 포함하는 <ul>만 찾아 처리
 for ol in ols:
@@ -29,4 +38,14 @@ for ol in ols:
         # <span class"x"> 안의 텍스트를 추출
         examples = ul.find_all('span', class_='x')
         for examples in examples:
-            print(examples.get_text())   # 예문 텍스트만 출력
+            examples_list.append(examples.get_text())  # 예문 텍스트를 리스트에 추가
+
+#print(examples.get_text())   # 예문 텍스트만 출력
+
+# pandas DataFrame을 사용하여 예문을 엑셀로 저장
+df = pd.DataFrame(examples_list, columns=["Examples"])
+
+# 예문을 엑셀 파일로 저장
+df.to_excel("examples.xlsx", index=False)
+
+print("Examples saved to 'examples.xlsx'")
