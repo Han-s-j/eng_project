afb00e4d869cde72840e33a20b2dffad86ab3a3a
Han-s-j <78011872+Han-s-j@users.noreply.github.com>
2025-04-04 18:43:16 +0900

    0404

--- DB/engProject.sql
+++ DB/engProject.sql
@@ -8,15 +8,39 @@ GRANT UNLIMITED TABLESPACE TO eng;
 
 CREATE TABLE word_list(
     word_num NUMBER PRIMARY KEY
-    ,word_list VARCHAR2(1000)
-    ,word_mean VARCHAR2(2000)
-    ,etymology VARCHAR2(4000)
-    ,ipk VARCHAR2(50)
-    ,phonetics VARCHAR2(1000)
+    ,word_eng VARCHAR2(1000)
+);
+
+CREATE TABLE audio_list(
+    word_num NUMBER
+    ,audio_num NUMBER PRIMARY KEY
+    ,audio_file VARCHAR2(100)
+    ,ipk_eng VARCHAR2(50)
+    ,CONSTRAINT fk_audio FOREIGN KEY (word_num) REFERENCES word_list(word_num) 
+);
+
+CREATE TABLE def_list(
+    word_num NUMBER
+    ,def_num NUMBER PRIMARY KEY
+    ,def_eng VARCHAR2(1000)
+    ,CONSTRAINT fk_def FOREIGN KEY (word_num) REFERENCES word_list(word_num)
+);
+
+CREATE TABLE ex_list(
+    def_num NUMBER
+    ,ex_num NUMBER PRIMARY KEY
+    ,ex_eng VARCHAR2(1000)
+    ,CONSTRAINT fk_ex FOREIGN KEY (def_num) REFERENCES def_list(def_num)
 );
 
 SELECT *
-FROM word_list
-ORDER BY word_num ASC;
+FROM word_list;
+SELECT *
+FROM audio;
+
+SELECT a.word_eng, def_eng, ex_eng
+FROM word_list a
+JOIN def_list b ON a.word_num = b.word_num
+JOIN ex_list c ON b.def_num = c.def_num;
 
 
