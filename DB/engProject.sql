084e5874385c917214b3900ffa9b52c70facd270
Han-s-j <78011872+Han-s-j@users.noreply.github.com>
2025-04-18 18:23:31 +0900

    0418

--- DB/engProject.sql
+++ DB/engProject.sql
@@ -6,6 +6,11 @@ GRANT CONNECT, RESOURCE TO eng;
 -- 테이블 스페이스 접근권한(물리적인 저장 차일)
 GRANT UNLIMITED TABLESPACE TO eng;
 
+CREATE TABLE az_list(
+    az_num NUMBER PRIMARY KEY
+    ,az_eng VARCHAR2(1)
+);
+
 CREATE TABLE word_list(
     word_num NUMBER PRIMARY KEY
     ,word_eng VARCHAR2(1000)
@@ -61,6 +66,45 @@ START WITH 1
 NOCACHE
 NOCYCLE;
 
+ALTER TABLE word_list ADD CONSTRAINT uq_word_eng UNIQUE (word_eng);
+
+CREATE TABLE user_word (
+    mem_id VARCHAR(100) NOT NULL                            -- 사용자 ID (members 테이블의 mem_id와 연결)
+    ,word_eng VARCHAR2(1000) NOT NULL                       -- 단어 ID (word_list 테이블의 word_eng와 연결)
+    ,study_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP         -- 학습 날짜 (선택 사항)
+    ,PRIMARY KEY (mem_id, word_eng)                         -- 복합 기본키
+    ,FOREIGN KEY (mem_id) REFERENCES members(mem_id)        -- member 테이블의 mem_id 참조
+    ,FOREIGN KEY (word_eng) REFERENCES word_list(word_eng)  -- word_list 테이블의 word_eng 참조
+);
+---------------------------------------------------
+SELECT *
+FROM user_word
+WHERE mem_id = 'su';
+
+SELECT mem_id, word_eng, study_date
+FROM user_word
+WHERE mem_id = 'su';
+
+INSERT INTO user_word (mem_id, word_eng)
+VALUES ('su', 'address');
+
+SELECT *
+FROM members;
+
+DELETE 
+FROM members;
+
+SELECT *
+FROM word_list;
+-- @로 시작하는 단어
+SELECT word_eng
+FROM word_list
+WHERE word_eng LIKE 'a%';
+
+SELECT a.word_eng, b.az_eng
+FROM az_list b
+JOIN word_list a ON LOWER(a.word_eng) LIKE CONCAT(LOWER(b.az_eng),'%')
+ORDER BY b.az_eng, a.word_eng;
 
 INSERT INTO members (mem_id, mem_pw, mem_nm)
 VALUES ('test', '1234', 'test');
@@ -70,36 +114,97 @@ VALUES ('1', sent_seq.NEXTVAL, 'Please note my change of address.', 'test');
 INSERT INTO my_sent_list (word_num, sent_num, my_sent, mem_id)
 VALUES ('1',sent_seq.NEXTVAL, 'Is that your home address?', 'test');
 
-SELECT word_num, sent_num, my_sent, mem_id, sent_reg_date, use_yn
-FROM my_sent_list;
---WHERE mem_id = 'test';
+-- 사용자 문장 조회
+select *
+from my_sent_list
+WHERE mem_id = 'su';
 
-SELECT a.word_eng, b.my_sent, c.mem_id
+-- 문장 데이터 삭제
+DELETE 
+FROM my_sent_list
+WHERE mem_id = 'su';
+
+DELETE 
+FROM my_sent_list;
+
+
+
+
+SELECT a.word_eng, b.my_sent,b.sent_num, c.mem_id
 FROM word_list a
     ,my_sent_list b
     , members c
 WHERE a.word_num =b.word_num
-AND   b.mem_id = c.mem_id;
---AND  a.word_eng = 'about'
---AND b.mem_id = 'test';
+AND   b.mem_id = c.mem_id
+AND  a.word_eng = 'address'
+AND b.mem_id = 'su'
+AND b.del_yn ='N'
+ORDER BY sent_reg_date DESC;
+
+SELECT a.word_eng, b.my_sent, b.sent_num, c.mem_id
+FROM word_list a
+JOIN my_sent_list b ON a.word_num = b.word_num
+JOIN members c ON b.mem_id = c.mem_id
+WHERE a.word_eng = 'address'
+  AND b.mem_id = 'su'
+  AND b.del_yn = 'N'
+ORDER BY b.sent_reg_date DESC;
+
+SELECT word_num, sent_num, my_sent, mem_id, sent_reg_date, use_yn
+FROM my_sent_list;
+--WHERE mem_id = 'test';
+
+-- 문장 전체 조회
+SELECT a.word_eng, b.word_num ,b.my_sent, c.mem_id, b.sent_reg_date
+FROM word_list a
+    ,my_sent_list b
+    , members c
+WHERE a.word_num =b.word_num
+AND   b.mem_id = c.mem_id
+--AND  b.word_num = 4;
+AND  a.word_eng = 'address'
+AND b.mem_id = 'test'
+ORDER BY sent_reg_date DESC;
 
 
+-- 문장 삭제
+UPDATE my_sent_list
+SET del_yn = 'Y'
+WHERE sent_num = 60
+AND mem_id = 'su';
 
-SELECT *
-FROM word_list;
+-- 컬럼명 변경
+ALTER TABLE my_sent_list
+RENAME COLUMN use_yn TO del_yn;
+-- DEFAULT 값 변경
+ALTER TABLE my_sent_list
+MODIFY del_yn VARCHAR2(1) DEFAULT 'N';
 
+-- word_list 학습완료 컬럼 추가
+ALTER TABLE word_list ADD study_yn VARCHAR2(1) DEFAULT 'N';
+-- myvoca 쿼리
 SELECT word_eng
 FROM word_list
-WHERE word_eng LIKE 'a%';
-
+WHERE study_yn = 'Y';
+    
+-- 학습 yn 변경
+UPDATE word_list
+SET study_yn = 'Y'
+WHERE word_eng = 'address';
+        
 SELECT
     word_num
     ,word_eng
 FROM word_list
 WHERE word_num = 1;
 
+-- 오디오
+SELECT a.word_eng, a.word_num, b.audio_num,b.audio_file,b.ipk_eng
+FROM word_list a,audio_list b
+WHERE a.word_num =b.word_num
+AND  a.word_num = 4;
 
-SELECT *
+SELECT ipk_eng
 FROM audio_list;
 
 -- 단어번호에 맞는 예문 출력
@@ -129,3 +234,16 @@ FROM word_list a
 WHERE a.word_num =b.word_num
 AND   b.def_num = c.def_num
 AND  a.word_eng ='about';
+
+SELECT a.word_eng, b.my_sent, c.mem_id
+FROM word_list a
+    ,my_sent_list b
+    , members c
+WHERE a.word_num =b.word_num
+AND   b.mem_id = c.mem_id
+AND  a.word_eng = 'address'
+AND b.mem_id = 'test'
+ORDER BY sent_reg_date DESC ;
+        
+CREATE OR REPLACE DIRECTORY sql_dir AS 'C:\sql_backup\';
+GRANT READ, WRITE ON DIRECTORY sql_dir TO eng;
